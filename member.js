b6334143ae352158da8191680f2e1cbbbffa49a7
Yoga Dimas Pambudi <60217207+Yogadimas@users.noreply.github.com>
2025-03-17 14:22:16 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = this;
+    member.skills = [
+        {name: "C++", level: 9},
+        {name: "Java", level: 8},
+        {name: "Python", level: 7},
+        {name: "JavaScript", level: 6}
+    ];
+}
